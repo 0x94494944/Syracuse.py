99db9a2d8dc7bfd8bf5824a7c867bcd7beb96f93
Felix-QC <yamax2019@gmail.com>
2020-05-27 23:16:09 -0400

    Update main.py

--- main.py
+++ main.py
@@ -5,12 +5,12 @@ import os
 def main():
     user_input = int(input("Enter a number >> "))
     os.system("cls")
-    print("Here is your answer:\n")
+    print("Here is your answer:")
     while user_input != 1:
         if user_input % 2 == 0:
             user_input = user_input // 2 
-            print(user_input)
+            print(">>", user_input)
         else:
             user_input = (user_input * 3) + 1  
-            print(user_input)
+            print(">>", user_input)
 main()
